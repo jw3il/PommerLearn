c50a9022887cbb53384fe82166b2cb2a9f908eda
Jannis Weil <jannis.weil@tu-darmstadt.de>
2020-10-14 12:00:13 +0200

    Added program options

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <array>
 
 #include "runner.h"
@@ -12,6 +12,10 @@
 #include "stateobj.h"
 
 #include "agents.hpp"
+
+#include "boost/program_options.hpp"
+
+namespace po = boost::program_options;
 
 void load_models()
 {
@@ -54,8 +58,10 @@ vector<unique_ptr<NeuralNetAPI>> create_new_net_batches(const string& modelDirec
     return netBatches;
 }
 
-void free_for_all_tourney(size_t nbGames, IPCManager* ipcManager)
+void free_for_all_tourney(long maxGames, long maxSamples, IPCManager* ipcManager)
 {
+    // TODO: Decouple agent creation
+
     StateConstants::init(false);
 #ifdef TENSORRT
     TensorrtAPI netSingle(0, 1, "model", "float32");
@@ -105,7 +111,7 @@ void free_for_all_tourney(size_t nbGames, IPCManager* ipcManager)
         new agents::SimpleAgent(rand()),
     };
 
-    Runner::run(agents, gameMode, 800, nbGames, -1, -1, false, ipcManager);
+    Runner::run(agents, gameMode, 800, maxGames, maxSamples, -1, false, ipcManager);
 }
 
 void generate_sl_data(int nbSamples, IPCManager* ipcManager)
@@ -119,23 +125,50 @@ void generate_sl_data(int nbSamples, IPCManager* ipcManager)
 }
 
 int main(int argc, char **argv) {
-    std::string dataPrefix = "data";
-    if (argc >= 2) {
-        dataPrefix = argv[1];
+    po::options_description configDesc("Available options");
+    configDesc.add_options()
+        ("help", "Print help message")
+        ("mode", po::value<std::string>()->default_value("ffa_sl"), "Available modes: ffa_sl, ffa_mcts")
+        ("max_games", po::value<int>()->default_value(10), "The max. number of generated games (ignored if -1)")
+        ("log", "If set, generate enough samples to fill a whole dataset (chunk_size * chunk_count samples)")
+        ("file_prefix", po::value<std::string>()->default_value("./data"), "Set the filename prefix for the new datasets")
+        ("chunk_size", po::value<int>()->default_value(1000), "Max. number of samples in a single file inside the dataset")
+        ("chunk_count", po::value<int>()->default_value(100), "Max. number of chunks in a dataset")
+    ;
+
+    po::variables_map configVals;
+    po::store(po::parse_command_line(argc, argv, configDesc), configVals);
+    po::notify(configVals);
+
+    if (configVals.count("help")) {
+        std::cout << configDesc << "\n";
+        return 1;
     }
 
-    int chunkSize = 1000;
-    int chunkCount = 100;
-
+    // check whether we want to log the games
     std::unique_ptr<FileBasedIPCManager> ipcManager;
+    int maxSamples;
+    if (configVals.count("log")) {
+        ipcManager = std::make_unique<FileBasedIPCManager>(configVals["file_prefix"].as<std::string>(), configVals["chunk_size"].as<int>(), configVals["chunk_count"].as<int>());
+        // fill at most one dataset
+        maxSamples = configVals["chunk_size"].as<int>() * configVals["chunk_count"].as<int>();
+    }
+    else {
+        // unlimited sampling
+        maxSamples = -1;
+    }
 
-    // uncomment to enable logging
-    // ipcManager = std::make_unique<FileBasedIPCManager>(dataPrefix, chunkSize, chunkCount);
+    int maxGames = configVals["max_games"].as<int>();
 
-    // generate enough steps to fill one dataset
-    // generate_sl_data(chunkCount * chunkSize, ipcManager.get());
-
-    free_for_all_tourney(1, ipcManager.get());
+    if (configVals["mode"].as<std::string>() == "ffa_sl") {
+        Runner::run_simple_agents(800, maxGames, maxSamples, -1, false, ipcManager.get());
+    }
+    else if (configVals["mode"].as<std::string>() == "ffa_mcts") {
+        free_for_all_tourney(maxGames, maxSamples, ipcManager.get());
+    }
+    else {
+        std::cerr << "Unknown mode" << std::endl;
+    }
 
     if(ipcManager.get() != nullptr)
     {
