Fixed metric normalization and added kl div masking (Closes #105)

pommerlearn/training/loss/masked_continious_cross_entropy.py
@@ -1,9 +1,10 @@
-from torch.nn.modules.loss import _WeightedLoss
+import torch
+from torch.nn.modules.loss import _Loss
 from torch import Tensor
 import torch.nn.functional as F
 
 
-class MaskedContiniousCrossEntropyLoss(_WeightedLoss):
+class MaskedContiniousCrossEntropyLoss(_Loss):
     """
     This criterion describes a cross entropy loss with continious values (non-one-hot-encoded) as targets.
 
@@ -18,8 +19,11 @@ class MaskedContiniousCrossEntropyLoss(_WeightedLoss):
     def __init__(self) -> None:
         super(MaskedContiniousCrossEntropyLoss, self).__init__()
 
-    def forward(self, input: Tensor, target: Tensor, mask: Tensor=None) -> Tensor:
+    def forward(self, log_input: Tensor, target: Tensor, mask: Tensor = None) -> Tensor:
         if mask is None:
-            return -(target * F.log_softmax(input, dim=-1)).sum(dim=-1).mean()
+            return -(target * F.log_softmax(log_input, dim=-1)).sum(dim=-1).mean()
         else:
-            return -(target * F.log_softmax(input, dim=-1) * mask).sum(dim=-1).mean()
+            assert len(log_input.shape) == 3, "Masking is currently only supported for sequences"
+
+            cross_entropy = -(target * F.log_softmax(log_input, dim=-1) * mask).sum()
+            return cross_entropy / mask.sum()

pommerlearn/training/loss/masked_mse.py
@@ -15,4 +15,4 @@ class MaskedMSELoss(_Loss):
         if mask is None:
             return F.mse_loss(input, target)
         else:
-            return torch.sum(torch.pow(input - target, 2) * mask) / torch.sum(mask)
+            return torch.sum(torch.pow(input - target, 2) * mask) / mask.sum()

pommerlearn/training/metrics.py
@@ -4,6 +4,7 @@ import torch
 from torch.nn.functional import kl_div, log_softmax
 
 from nn.PommerModel import PommerModel
+from training.loss.masked_kl_div import MaskedKLDivergenceLoss
 
 
 class Metrics:
@@ -18,6 +19,7 @@ class Metrics:
         self.sum_policy_kl = 0
         self.sum_value_loss = 0
         self.steps = 0
+        self.kl = MaskedKLDivergenceLoss()
 
     def reset(self):
         self.correct_cnt = 0
@@ -28,8 +30,8 @@ class Metrics:
         self.sum_value_loss = 0
         self.steps = 0
 
-    def update(self, model: PommerModel, policy_loss, value_loss, value_loss_ratio, x_train, yv_train, ya_train, yp_train,
-                       fit_pol_dist, normalize_loss_nb_samples: Optional[int] = None, model_state=None, ids=None, device=None):
+    def update(self, model: PommerModel, policy_loss, value_loss, value_loss_ratio, x_train, yv_train, ya_train, yp_train
+               , model_state=None, ids=None, device=None):
         """
         Updates the metrics and calculates the combined loss
 
@@ -41,76 +43,59 @@ class Metrics:
         :param yv_train: Target labels for value
         :param ya_train: Target labels for policy
         :param yp_train: Target policy distribution
-        :param fit_pol_dist: Whether to use the policy distribution for the policy loss target
-        :param normalize_loss_nb_samples: Whether to normalize the calculated losses according to some default number
-                                          of samples. This is important when you have varying number of samples.
         :param model_state: The current state of the model (optional)
         :param ids: The episode ids in the data (required for masking)
         :param device: The device (required for masking)
         :return: A tuple of the combined loss and (optionally) the next state
         """
-
-        # get the combined loss
-        # TODO: Create a separate method instead?
-        if model.is_stateful:
-            value_out, policy_out, next_state = model(model.flatten(x_train, model_state))
-        else:
-            value_out, policy_out = model(model.flatten(x_train, None))
-
         mask = None
         if ids is not None:
             mask = (ids != -1).to(dtype=torch.float).unsqueeze(-1)
             if device is not None:
                 mask = mask.to(device=device)
 
-        # TODO: Improve code design, this should be handled automatically
-        if fit_pol_dist:
-            cur_policy_loss = policy_loss(policy_out, yp_train, mask=mask)
-        else:
-            cur_policy_loss = policy_loss(policy_out, ya_train, mask=mask)
-
-        cur_value_loss = value_loss(value_out, yv_train.unsqueeze(-1), mask=mask)
-        combined_loss = (1 - value_loss_ratio) * cur_policy_loss + value_loss_ratio * cur_value_loss
-
-        # update metrics
-        _, pred_label = torch.max(policy_out.data, -1)
-
-        if len(x_train.data.size()) == 5:
-            self.total_cnt += x_train.data.size()[0] * x_train.data.size()[1]
-        else:
-            self.total_cnt += x_train.data.size()[0]
-
-        if normalize_loss_nb_samples is None:
-            normalization_factor = 1
-        else:
-            if len(x_train.shape) == 4:
-                # regular batch
-                nb_samples = x_train.shape[0]
-            elif len(x_train.shape) == 5:
-                # sequence
+        if len(x_train.shape) == 4:
+            # regular batch
+            nb_samples = x_train.shape[0]
+        elif len(x_train.shape) == 5:
+            # sequence
+            if mask is None:
                 nb_samples = x_train.shape[0] * x_train.shape[1]
             else:
-                raise ValueError(f"Unsupported input dimension! Got shape: {x_train.shape}")
-
-            normalization_factor = float(nb_samples) / normalize_loss_nb_samples
-
-        self.correct_cnt += float((pred_label == ya_train.data).sum())
-
-        self.sum_policy_loss += normalization_factor * float(cur_policy_loss.data)
-        # note: kl value ignores mask
-        kl_value = kl_div(log_softmax(policy_out, dim=-1), yp_train, reduction='batchmean')
-        self.sum_policy_kl += normalization_factor * float(kl_value)
-        self.sum_value_loss += normalization_factor * float(cur_value_loss.data)
-        self.sum_combined_loss += normalization_factor * float(combined_loss.data)
-
-        self.steps += normalization_factor
-
-        weighted_loss = combined_loss * normalization_factor
+                nb_samples = torch.sum(mask)
+        else:
+            raise ValueError(f"Unsupported input dimension! Got shape: {x_train.shape}")
 
         if model.is_stateful:
-            return weighted_loss, next_state
+            value_out, policy_out, next_state = model(model.flatten(x_train, model_state))
         else:
-            return weighted_loss, None
+            value_out, policy_out = model(model.flatten(x_train, None))
+
+        # get the combined loss
+        cur_policy_loss = policy_loss(policy_out, yp_train, mask=mask)
+        cur_value_loss = value_loss(value_out, yv_train.unsqueeze(-1), mask=mask)
+        # combined_loss = (1 - value_loss_ratio) * cur_policy_loss + value_loss_ratio * cur_value_loss
+        combined_loss = cur_policy_loss + cur_value_loss * 0
+
+        # update metrics
+        self.total_cnt += nb_samples
+        _, pred_label = torch.max(policy_out.data, -1)
+        if mask is None:
+            self.correct_cnt += float((pred_label == ya_train.data).sum())
+        else:
+            self.correct_cnt += float(((pred_label == ya_train.data) * mask.squeeze(-1)).sum())
+
+        self.sum_policy_loss += float(cur_policy_loss.data)
+        self.sum_policy_kl += float(self.kl(policy_out, yp_train, mask=mask))
+        self.sum_value_loss += float(cur_value_loss.data)
+        self.sum_combined_loss += float(combined_loss.data)
+
+        self.steps += 1
+
+        if model.is_stateful:
+            return combined_loss, next_state
+        else:
+            return combined_loss, None
 
     def combined_loss(self):
         return self.sum_combined_loss / self.steps

pommerlearn/training/train_cnn.py
@@ -49,7 +49,7 @@ def create_model(train_config):
                        slice_scalars=train_config["slice_scalars"],
                        # flat arguments
                        use_flat_core=train_config["use_flat_core"],
-                       core_hidden=128, value_hidden=64, policy_hidden=64,
+                       core_hidden=256, value_hidden=64, policy_hidden=128,
                        # .. with lstm
                        use_lstm=train_config["use_lstm"], lstm_layers=1
         )
@@ -90,12 +90,7 @@ def train_cnn(train_config):
         print(f"Loading torch state from {str(model_input_dir)}")
         load_torch_state(model, optimizer, str(get_torch_state_path(model_input_dir)))
 
-    fit_pol_dist = train_config["fit_policy_distribution"]
-    if fit_pol_dist:
-        policy_loss = MaskedContiniousCrossEntropyLoss()
-    else:
-        policy_loss = nn.CrossEntropyLoss()
-
+    policy_loss = MaskedContiniousCrossEntropyLoss()
     value_loss = MaskedMSELoss()
 
     total_it = len(train_loader) * train_config["nb_epochs"]
@@ -112,7 +107,7 @@ def train_cnn(train_config):
 
     global_step_start = train_config["global_step"]
     global_step_end = run_training(model, train_config["nb_epochs"], optimizer, lr_schedule, momentum_schedule,
-                                   value_loss, policy_loss, fit_pol_dist, train_config["value_loss_ratio"],
+                                   value_loss, policy_loss, train_config["value_loss_ratio"],
                                    train_loader, val_loader, use_cuda, log_dir, global_step=global_step_start)
 
     base_dir = Path(train_config["output_dir"])
@@ -286,7 +281,7 @@ def export_as_script_module(model, batch_size, dummy_input, dir) -> None:
 
 
 def run_training(model: PommerModel, nb_epochs, optimizer, lr_schedule, momentum_schedule, value_loss, policy_loss,
-                 fit_pol_dist, value_loss_ratio, train_loader, val_loader, use_cuda, log_dir, global_step=0):
+                 value_loss_ratio, train_loader, val_loader, use_cuda, log_dir, global_step=0):
     """
     Trains a given model for a number of epochs
 
@@ -296,7 +291,6 @@ def run_training(model: PommerModel, nb_epochs, optimizer, lr_schedule, momentum
     :param lr_schedule: LR-scheduler
     :param momentum_schedule: Momentum scheduler
     :param policy_loss: Policy loss object
-    :param fit_pol_dist: Whether to use the policy distribution target for the policy loss
     :param value_loss: Value loss object
     :param value_loss_ratio: Value loss ratio
     :param train_loader: Training data loader
@@ -356,7 +350,7 @@ def run_training(model: PommerModel, nb_epochs, optimizer, lr_schedule, momentum
                 model.set_input_options(sequence_length=None, has_state_input=False)
 
             combined_loss, _ = m_train.update(model, policy_loss, value_loss, value_loss_ratio, x_train, yv_train,
-                                           ya_train, yp_train, fit_pol_dist, ids=ids, device=device)
+                                           ya_train, yp_train, ids=ids, device=device)
 
             combined_loss.backward()
             for param_group in optimizer.param_groups:
@@ -380,10 +374,10 @@ def run_training(model: PommerModel, nb_epochs, optimizer, lr_schedule, momentum
                 if val_loader is not None:
                     model.eval()
                     if model.is_stateful:
-                        m_val = get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist,
+                        m_val = get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss,
                                                   use_cuda, val_loader)
                     else:
-                        m_val = get_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist, use_cuda,
+                        m_val = get_val_loss(model, value_loss_ratio, value_loss, policy_loss, use_cuda,
                                              val_loader)
 
                     m_val.log_to_tensorboard(writer_val, global_step)
@@ -405,7 +399,7 @@ def run_training(model: PommerModel, nb_epochs, optimizer, lr_schedule, momentum
     return global_step
 
 
-def get_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist, use_cuda, data_loader) -> Metrics:
+def get_val_loss(model, value_loss_ratio, value_loss, policy_loss, use_cuda, data_loader) -> Metrics:
     """
     Returns the validation metrics by evaluating it on the full validation dataset
 
@@ -413,7 +407,6 @@ def get_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist,
     :param value_loss_ratio: Value loss ratio
     :param value_loss: Value loss object
     :param policy_loss: Policy loss object
-    :param fit_pol_dist: Whether to use the policy distribution for the policy loss target
     :param use_cuda: Boolean whether GPU is used
     :param data_loader: Data loader object (e.g. val_loader)
     :return: Updated metric object
@@ -428,12 +421,12 @@ def get_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist,
         x, ya_val = Variable(x, requires_grad=False), Variable(ya_val, requires_grad=False)
 
         model.set_input_options(sequence_length=None, has_state_input=False)
-        m_val.update(model, policy_loss, value_loss, value_loss_ratio, x, yv_val, ya_val, yp_val, fit_pol_dist)
+        m_val.update(model, policy_loss, value_loss, value_loss_ratio, x, yv_val, ya_val, yp_val)
 
     return m_val
 
 
-def get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_pol_dist, use_cuda, data_loader,
+def get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss, use_cuda, data_loader,
                  verbose = False) -> Metrics:
     """
     Returns the validation metrics by evaluating it on the full validation dataset
@@ -442,7 +435,6 @@ def get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_
     :param value_loss_ratio: Value loss ratio
     :param value_loss: Value loss object
     :param policy_loss: Policy loss object
-    :param fit_pol_dist: Whether to use the policy distribution for the policy loss target
     :param use_cuda: Boolean whether GPU is used
     :param data_loader: Data loader object (e.g. val_loader)
     :param verbose: Whether to output debugging info
@@ -508,8 +500,7 @@ def get_stateful_val_loss(model, value_loss_ratio, value_loss, policy_loss, fit_
             # update loss & get next state
             model.set_input_options(sequence_length=(until_idx - current_idx), has_state_input=True)
             loss, next_state = m_val.update(model, policy_loss, value_loss, value_loss_ratio, obs_part, val_part,
-                                            act_part, pol_part, fit_pol_dist, model_state=model_state,
-                                            normalize_loss_nb_samples=data_loader.batch_size)
+                                            act_part, pol_part, model_state=model_state)
 
             current_idx = until_idx
             if reset_state:
@@ -574,12 +565,11 @@ def fill_default_config(train_config):
         "value_loss_ratio": 0.1,
         "test_size": 0.2,
         "batch_size": 128,  # warning: should be adapted when using sequences
-        "batch_size_test": 128,
+        "batch_size_test": 1024,
         "random_state":  42,
         "nb_epochs":  20,
         "model": "risev3",  # "a0", "risev3", "lstm"
         "sequence_length": 8,  # only used when model is stateful
-        "fit_policy_distribution": True,
         "use_downsampling": True,
         "se_type": None,
         "num_res_blocks": 4,