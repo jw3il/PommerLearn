4741bfea2dc5bd3dac3508b025f6303f06780591
Jannis Weil <jannis.weil@tu-darmstadt.de>
2022-12-30 15:03:23 +0100

    Added terminal solver to search experiments script

--- docker/search_experiments.sh
+++ docker/search_experiments.sh
@@ -13,23 +13,24 @@ fi
 # define gpus to be used as a list
 # simply use gpu indices multiple times if you want to schedule
 # multiple jobs per gpu 
-GPUS="0,0"
+GPUS="0,0,1,1,2,2,3,3"
 
 # model paths within the docker containers
 declare -a MODEL_NAME_PATH_PAIRS=(
-    "trained /data/2022_12_14-16_06_21_from_zero_new_planes_e1_i200_199_model/onnx"
-    "initial /data/dummy-model/onnx"
+    "sl /data/model-sl/onnx"
+    "dummy /data/model-dummy/onnx"
 )
 
-GAMES=10 #1000
-SIMULATIONS="0 50" # "0 50 100 250 500 1000"
-SEARCH_MODES="OnePlayer TwoPlayer" # "OnePlayer TwoPlayer"
+GAMES=1000
+SIMULATIONS="0 50 100 250 500 1000"
+SEARCH_MODES="OnePlayer TwoPlayer"
+TERMINAL_SOLVER="true false"
 
 # logfile path on host machine
 LOGFILE="${POMMER_DATA_DIR}/$(date +%Y%m%d_%H%M%S)_pommer_log.csv"
 
 # create logfile and write column headers
-echo "SearchMode,ModelName,ModelPath,Simulations,Episodes,TotalSteps,Wins0,Alive0,Wins1,Alive1,Wins2,Alive2,Wins3,Alive3,Draws,NotDone,Time" > "${LOGFILE}"
+echo "SearchMode,TerminalSolver,ModelName,ModelPath,Simulations,Episodes,TotalSteps,Wins0,Alive0,Wins1,Alive1,Wins2,Alive2,Wins3,Alive3,Draws,NotDone,Time" > "${LOGFILE}"
 
 # TODO: Add terminal solver option
 
@@ -37,6 +38,7 @@ echo "SearchMode,ModelName,ModelPath,Simulations,Episodes,TotalSteps,Wins0,Alive
 for sim in ${SIMULATIONS}; do
 for pair in "${MODEL_NAME_PATH_PAIRS[@]}"; do
 for search in ${SEARCH_MODES}; do
+for tsolver in ${TERMINAL_SOLVER}; do
     read -a strarr <<< "$pair"
     model_name=${strarr[0]}
     model_path=${strarr[1]}
@@ -54,9 +56,10 @@ for search in ${SEARCH_MODES}; do
     fi
 
     # main command
-    CMD="$EXEC --gpu \$CUDA_VISIBLE_DEVICES --mode=ffa_mcts --model=${model_path} ${SIM_PARAM} --max-games=${GAMES}"
+    CMD="$EXEC --gpu \$CUDA_VISIBLE_DEVICES --mode=ffa_mcts --model=${model_path} ${SIM_PARAM} --max-games=${GAMES} --use-terminal-solver=${tsolver}"
     # get stats from last line and append stats with run configuration to logfile
-    echo "$CMD | tail -n 1 | xargs printf \"${search},${model_name},${model_path},${sim},%s\n\" >> ${LOGFILE}";
+    echo "$CMD | tail -n 1 | xargs printf \"${search},${tsolver},${model_name},${model_path},${sim},%s\n\" >> ${LOGFILE}";
+done
 done
 done
 done | simple_gpu_scheduler --gpus $GPUS
