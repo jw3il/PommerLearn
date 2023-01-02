e6b7dd41c12babe49472117634b9d74e03155fef
Jannis Weil <jannis.weil@tu-darmstadt.de>
2023-01-02 12:36:16 +0100

    Also log stderr to the files

--- docker/rl_experiments.sh
+++ docker/rl_experiments.sh
@@ -20,11 +20,11 @@ DATE=$(date +%Y%m%d_%H%M%S)
 (
 for model in "dummy" "sl"; do
 cat << EndOfMessage
-bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_default" --it 200 > "${DATE}_${model}_default.log"
-bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_default_1e" --it 200 --exec-args="--env-gen-seed-eps=1" > "${DATE}_${model}_default_1e.log"
-bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent" --it 200 --exec-args="--1st-opponent-type=RawNetAgent" > "${DATE}_${model}_raw_net_opponent.log"
-bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent_and_planning" --it 200 --exec-args="--1st-opponent-type=RawNetAgent --planning-agents=RawNetAgent" > "${DATE}_${model}_raw_net_opponent_and_planning.log"
-bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent_and_planning_sd_2" --it 200 --exec-args="--1st-opponent-type=RawNetAgent --planning-agents=RawNetAgent --switch-depth=2" > "${DATE}_${model}_raw_net_opponent_and_planning_sd_2.log"
+bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_default" --it 200 > "${DATE}_${model}_default.log" 2>&1
+bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_default_1e" --it 200 --exec-args="--env-gen-seed-eps=1" > "${DATE}_${model}_default_1e.log" 2>&1
+bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent" --it 200 --exec-args="--1st-opponent-type=RawNetAgent" > "${DATE}_${model}_raw_net_opponent.log" 2>&1
+bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent_and_planning" --it 200 --exec-args="--1st-opponent-type=RawNetAgent --planning-agents=RawNetAgent" > "${DATE}_${model}_raw_net_opponent_and_planning.log" 2>&1
+bash run.sh --gpu \$CUDA_VISIBLE_DEVICES --name-initials=JW --model="/data/model-${model}/" --comment="${model}_raw_net_opponent_and_planning_sd_2" --it 200 --exec-args="--1st-opponent-type=RawNetAgent --planning-agents=RawNetAgent --switch-depth=2" > "${DATE}_${model}_raw_net_opponent_and_planning_sd_2.log" 2>&1
 EndOfMessage
 done
 ) | simple_gpu_scheduler --gpus $GPUS
